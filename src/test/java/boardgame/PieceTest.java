14b3362e49bb48688108d5d82a3e32428792c6dc
Willian Costa <willian95@outlook.com>
2024-04-29 13:19:08 -0300

    test: Teste unitário para classe Piece

---
+++ src/test/java/boardgame/PieceTest.java
@@ -0,0 +1,24 @@
+package boardgame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PieceTest {
+
+	@DisplayName("When Create APeace Should Return A Null Position and A Board")
+	@Test
+	void testPeice_whenCreateAPeace_ShouldReturnANullPositionAndABoard() {
+		// Given / Arrange
+		Board board = new Board();
+		
+		// When / Act
+		Piece piece = new Piece(board);
+		
+		// Then / Assert
+		assertEquals(piece.getBoard(), board, () -> "Should return the Board");
+		assertNull(piece.position, () -> "Position should be null");
+	}
+
+}
