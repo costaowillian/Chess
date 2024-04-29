28e373cb47adafd1ede3283fa3cd9f55821aaaa9
Willian Costa <willian95@outlook.com>
2024-04-29 15:05:12 -0300

    feat: colocando peças no tabuleiro

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,18 +11,23 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		this.board = new Board(8, 8);
+		this.initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
-			for (int j = 0; i < board.getColumns(); i++ ) {
+			for (int j = 0; j < board.getColumns(); j++ ) {
 				mat[i][j] = (ChessPiece) board.piece(i,j);
 			}
 		}
 		return mat;
 	}
 	
-	
+	private void initialSetup () {
+		board.placePiece(new Rook(this.board, Color.WHITE), new Position(2,1));
+		board.placePiece(new King(this.board, Color.BLACK), new Position(0,4));
+		board.placePiece(new King(this.board, Color.WHITE), new Position(7,4));
+	}
 
 }
