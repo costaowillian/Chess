3da75f5d867577361a0c640ba61ad5c4626d6da2
Willian Costa <willian95@outlook.com>
2024-04-29 14:26:22 -0300

    fix: correção do print Tabuleiro

--- src/main/java/application/UI.java
+++ src/main/java/application/UI.java
@@ -6,22 +6,22 @@ public class UI {
 
 	public static void printBoard (ChessPiece[][] pieces) {
 		for(int i = 0; i < pieces.length; i++) {
-			System.out.println(8 - i + " ");
-			for (int j = 0; i < pieces.length; j++) {
+			System.out.print(8 - i + " ");
+			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d f g h");
+		System.out.print("  a b c d e f g h");
 	}
 
 	private static void printPiece(ChessPiece piece) {
-		if(piece==null) {
-			System.out.println("-");
+		if(piece == null) {
+			System.out.print("-");
 		} else {
-			System.out.println(piece);
+			System.out.print(piece);
 		}
-		System.out.println(" ");
+		System.out.print(" ");
 	}
 
 }
