f097641b4ea595553f337c1cfeeda0881d2114a6
Willian Costa <willian95@outlook.com>
2024-04-29 14:10:53 -0300

    feat: add class UI

---
+++ src/test/java/chess/ChessMatchTest.java
@@ -0,0 +1,19 @@
+package chess;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChessMatchTest {
+
+
+	@DisplayName("Create A Chess Match Shoudl Return The Pieces Of The Match")
+	@Test
+	void testChessMath_whenCreate_ShouldReturnThePiecesOfTheMatch() {
+		// Given / Arrange
+		ChessMatch chessMatch = new ChessMatch();
+		// When / Act
+		
+		// Then / Assert
+	}
+
+}
